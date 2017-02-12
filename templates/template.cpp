d946730336ebbf3e8d0d686c68d5096f6af61593
Nikita Boyarskikh <N02@yandex.ru>
2017-02-12 13:07:30 +0300

    Add code templates

---
+++ templates/template.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <ctype.h>
+#include <deque>
+#include <iterator>
+#include <queue>
+#include <cstring>
+#include <set>
+#include <list>
+#include <map>
+#include <stack>
+#include <ctime>
+#include <cassert>
+#include <bitset>
+#include <stdio.h>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+typedef long int li;
+typedef std::vector<int> vi;
+typedef std::vector<bool> vb;
+typedef std::vector<string> vs;
+typedef std::vector<double> vd;
+typedef std::vector<long long> vll;
+typedef std::vector<long int> vli;
+typedef std::vector<std::vector<int> > vvi;
+typedef std::vector<std::vector<bool> > vvb;
+typedef std::vector<std::pair<int, int> > vpi;
+typedef std::vector<std::pair<long int, long int> > vpli;
+typedef std::pair<int, int> pi;
+typedef std::pair<ll, ll> pll;
+typedef std::vector<pll> vpll;
+
+#define all(c) (c).begin(),(c).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sz(c) (int)(c).size()
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define RFOR(i, a, b) for(int i = a-1; i >= b; i--)
+
+//#define TXT_FILE_IO
+//#define MY_DEBUG
+
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+
+int main()
+{
+#ifdef TXT_FILE_IO
+    if(!freopen("input.txt", "r", stdin)) cout<<"ERROR OPEN INPUT FILE!";
+    if(!freopen("output.txt", "w", stdout)) cout<<"ERROR OPEN OUTPUT FILE!";
+#endif //TXT_FILE_IO
+
+	
+
+#ifdef MY_DEBUG
+    system("pause");
+#endif //MY_DEBUG
+    
+#ifdef TXT_FILE_IO
+    fclose(stdin);
+    fclose(stdout);
+#endif //TXT_FILE_IO
+    return 0;
+}
