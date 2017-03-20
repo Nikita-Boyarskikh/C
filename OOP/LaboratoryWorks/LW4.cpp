719093a98c374b31a5903e904c39e9f09cd221bb
Nikita Boyarskikh <N02@yandex.ru>
2017-03-20 21:56:15 +0300

    Small refactoring in LW4.cpp

--- OOP/LaboratoryWorks/LW4.cpp
+++ OOP/LaboratoryWorks/LW4.cpp
@@ -8,15 +8,19 @@ int main(int argc, char **argv) {
     cout<<"Введите строку для проверки баланса скобок: "<<endl;
     cin>>str;
 
+    // Начальная иницализация
     bool find = false, done = false;
     char *open_p = NULL, *close_p = NULL;
     int checker = 0;
     int counter = 0;
-    for(int i=0; str[i] != '\0'; i++) {
+
+    // Считываем строку либо до конца, либо пока не будет перевеса закрывающих скобок
+    for(int i=0; str[i] != '\0' && checker >= 0; i++) {
         switch(str[i]) {
           case '(':
             counter++;
             checker++;
+            // Нашли вторую открывающую скобку, сбрасываем счётчик
             if(!find && counter == 2) {
                 open_p = str + i;
                 find = true;
@@ -24,6 +28,7 @@ int main(int argc, char **argv) {
             }
             break;
           case ')':
+            // Нашли соответствующую 2-ой открывающей закрывающую скобку
             if(!done && find && counter == 0) {
                 close_p = str + i;
                 done = true;
@@ -31,13 +36,14 @@ int main(int argc, char **argv) {
             if(find) counter--;
             checker--;
         }
-        if(checker < 0) break;
     }
 
+    // Проверяем сбалансированность
     if(checker != 0)
         cout<<"ОШИБКА: Скобки НЕ сбалансированы!"<<endl;
     else {
         cout<<"Скобки сбалансированы."<<endl;
+        // Выводим содержимое между второй парой скобок, если это возможно
         if(open_p && close_p && open_p < close_p) {
             cout<<"Между 2-ой парой скобок находится следующее: \"";
             for(char *i = open_p + 1; i < close_p; i++) cout<<*i;
